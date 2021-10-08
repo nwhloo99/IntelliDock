98c18e877054550983b34f3dc9ddd1f2825b4be0
nwhloo99 <77220827+nwhloo99@users.noreply.github.com>
2021-10-08 20:55:46 +0800

    Create constants file

---
+++ chill/lib/screens/constants.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+// Colors to be used in the app
+const kPrimaryColor = Color(0xFF4120A9);
+const kSecondaryColor = Color(0xFFA16AE8);
+const kTextColor = Color(0xFFDFDFFF);
+
+// FontSize
+const int kHeaderFont = 30;
+const int kTextFont = 18;
+
+// Padding
+const double kDefaultPadding = 20.0;
