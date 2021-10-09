9ca5d262e1a721649075d065dbf09b1f23d581a3
tchiong <57283702+tchiong@users.noreply.github.com>
2021-10-10 02:43:25 +0800

    Fix Model Bugs

--- chill/lib/main.dart
+++ chill/lib/main.dart
@@ -1,11 +1,11 @@
 import 'package:chill/utils/constants.dart';
 import 'package:chill/model/hauler/hauler.dart';
 import 'package:chill/model/model.dart';
-import 'package:chill/model/warehouse/warehouse.dart';
 import 'package:chill/queueManager/requests/request.dart';
 import 'package:chill/queueManager/user/hauler_user.dart';
 import 'package:chill/queueManager/user/warehouse_user.dart';
 import 'package:chill/screens/login/login_screen.dart';
+import 'package:chill/utils/template_models.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
@@ -22,8 +22,11 @@ void main() {
 class MyApp extends StatelessWidget {
   // Example HaulerUser object
   HaulerUser haulerUser = new HaulerUser(new Hauler(42069));
-  WarehouseUser bayUser = new WarehouseUser(new Warehouse('A', 10));
-  Model simulatedModel = new Model([17, 17, 8, 8, 21]);
+  WarehouseUser bayUser = new WarehouseUser(generateModelA()
+      .map
+      .warehouses
+      .firstWhere((element) => element.warehouseName == ('A')));
+  Model simulatedModel = generateModelA();
 
   // This widget is the root of your application.
   @override
