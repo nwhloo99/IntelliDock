c821a8cc52519cf6b4667885a4e936545b8b6c9c
nwhloo99 <77220827+nwhloo99@users.noreply.github.com>
2021-10-09 10:48:46 +0800

    Change DockingBayCard into inkWell that can perform onPressed functions

--- chill/lib/screens/hauler_dashboard/components/docking_bay_card.dart
+++ chill/lib/screens/hauler_dashboard/components/docking_bay_card.dart
@@ -1,7 +1,6 @@
 import 'package:chill/screens/constants.dart';
 import 'package:chill/screens/hauler_dashboard/components/entry_text.dart';
 import 'package:flutter/material.dart';
-import 'package:intl/intl.dart';
 
 class DockingBayCard extends StatefulWidget {
   DockingBayCard(
@@ -24,21 +23,33 @@ class DockingBayCard extends StatefulWidget {
 class _DockingBayCardState extends State<DockingBayCard> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-        padding: EdgeInsets.symmetric(vertical: 5),
-        margin: EdgeInsets.symmetric(vertical: 10, horizontal: 10),
-        decoration: BoxDecoration(
-            borderRadius: BorderRadius.all(Radius.circular(88)),
-            color: kSecondaryColor),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: <Widget>[
-            EntryText(data: "Docking bay: " + this.widget.dockingBay),
-            EntryText(
-                data: "Est Time Remaining: " +
-                    this.widget.estTime.inMinutes.toString() +
-                    " mins")
-          ],
-        ));
+    return ClipRRect(
+        child: Container(
+            margin: EdgeInsets.symmetric(horizontal: 10),
+            child: Material(
+                color: Color(0x3FBFBFBF),
+                child: InkWell(
+                    splashColor: kSecondaryColor,
+                    onLongPress: () {
+                      print("hi");
+                    },
+                    child: Container(
+                        padding: EdgeInsets.symmetric(vertical: 5),
+                        margin:
+                            EdgeInsets.symmetric(vertical: 10, horizontal: 10),
+                        decoration: BoxDecoration(
+                            borderRadius: BorderRadius.all(Radius.circular(88)),
+                            color: kSecondaryColor),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          children: <Widget>[
+                            EntryText(
+                                data: "Docking bay: " + this.widget.dockingBay),
+                            EntryText(
+                                data: "Est Time Remaining: " +
+                                    this.widget.estTime.inMinutes.toString() +
+                                    " mins")
+                          ],
+                        ))))));
   }
 }
