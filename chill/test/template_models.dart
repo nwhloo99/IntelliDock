46e1c794ba53c47f7a7d1325dea59290f245c02b
tchiong <57283702+tchiong@users.noreply.github.com>
2021-10-10 02:11:17 +0800

    Add ModelA

--- chill/test/template_models.dart
+++ chill/test/template_models.dart
@@ -1,1 +1,14 @@
+import 'package:chill/model/hauler/hauler.dart';
+import 'package:chill/model/model.dart';
 
+/** MODEL A
+ * WareHouse A: 3 Docking, Warehouse B: 2 Docking
+ * Hauler 1 travelling to unload at A,1
+ * Hauler 2 travelling to load at B,2
+ */
+Model generateModelA() {
+  Model templateModelA = new Model([3, 2]);
+  templateModelA.map.hauler_travelling_to_unload(new Hauler(1), 'A', 1);
+  templateModelA.map.hauler_travelling_to_load(new Hauler(2), 'B', 2);
+  return templateModelA;
+}
