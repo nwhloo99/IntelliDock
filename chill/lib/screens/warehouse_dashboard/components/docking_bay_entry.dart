a667b62ed37115ea600dd813273ea42ae4a23892
nwhloo99 <77220827+nwhloo99@users.noreply.github.com>
2021-10-09 18:01:43 +0800

    Add request button to DockingBayEntry

--- chill/lib/screens/warehouse_dashboard/components/docking_bay_entry.dart
+++ chill/lib/screens/warehouse_dashboard/components/docking_bay_entry.dart
@@ -2,6 +2,7 @@ import 'package:chill/model/warehouse/docking_bay.dart';
 import 'package:chill/screens/constants.dart';
 import 'package:chill/screens/widgets/entry_text.dart';
 import 'package:chill/screens/widgets/list_header.dart';
+import 'package:chill/screens/widgets/navigation_button.dart';
 import 'package:flutter/material.dart';
 
 class DockingBayEntry extends StatefulWidget {
@@ -21,20 +22,23 @@ class _DockingBayEntryState extends State<DockingBayEntry> {
             border: Border.all(color: kPrimaryColor),
             borderRadius: BorderRadius.all(Radius.circular(30))),
         margin: EdgeInsets.symmetric(horizontal: 10, vertical: 10),
-        padding: EdgeInsets.symmetric(horizontal: 20, vertical: 5),
-        child: Column(children: <Widget>[
-          ListHeader(header: this.widget.dockingBay.bayName),
-          EntryText(
-              data: "Status: " +
-                  (this.widget.dockingBay.isOccupied ? "In use" : "Free")),
-          // TODO
-          // HAULER FROM QUEUE OF HAULERS ARRIVING
-          EntryText(data: "Haulers arriving: "),
-          // TODO
-          // Add ETA of Hauler
-          EntryText(data: "ETA: -"),
-          EntryText(data: "Hauler at bay: "),
-          EntryText(data: "Time till available: 0 min")
-        ]));
+        padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+        child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: <Widget>[
+              ListHeader(header: this.widget.dockingBay.bayName),
+              EntryText(
+                  data: "Status: " +
+                      (this.widget.dockingBay.isOccupied ? "In use" : "Free")),
+              // TODO
+              // HAULER FROM QUEUE OF HAULERS ARRIVING
+              EntryText(data: "Haulers arriving: "),
+              // TODO
+              // Add ETA of Hauler
+              EntryText(data: "ETA: -"),
+              EntryText(data: "Hauler at bay: "),
+              EntryText(data: "Time till available: 0 min"),
+              NavigationButton(label: "Request", onPressed: () {})
+            ]));
   }
 }
